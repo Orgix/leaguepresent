257ae079e284baa769064c05e01bb5163be5593b
Orgix <tnikos28@gmail.com>
2023-04-22 17:14:48 +0300

    :rocket: transfered helper functions to separate file and imported to main

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-import {foo} from './helper.js'
+import {stringCapitalize, determineColor, formatDateTime, getTimeAgo} from './helper.js'
 
 let data;
 let divRes = document.getElementById("result");
@@ -12,8 +12,6 @@ async function getSummoner(summonerName,Region){
     return data;
 }
 
-
-foo("o")
 document.getElementById("sumName").addEventListener("keydown",async (ev)=>{
     
     if(ev.key === "Enter"){
@@ -273,64 +271,4 @@ function createSummonerCard(summonerData) {
     return card_result;
   }
 
-  function getTimeAgo(timestamp) {
-    // Get the current time in milliseconds
-    const currentTime = new Date().getTime();
-    
-    // Calculate the time elapsed since the game started
-    const timeElapsed = currentTime - timestamp;
-    
-    // Convert the time elapsed to seconds
-    const timeElapsedSeconds = Math.floor(timeElapsed / 1000);
-    
-    // Convert the time elapsed to minutes
-    const timeElapsedMinutes = Math.floor(timeElapsedSeconds / 60);
-    
-    // Convert the time elapsed to hours
-    const timeElapsedHours = Math.floor(timeElapsedMinutes / 60);
-    
-    // Convert the time elapsed to days
-    const timeElapsedDays = Math.floor(timeElapsedHours / 24);
-    
-    // Return the time elapsed as a human-readable string
-    if (timeElapsedDays > 0 && timeElapsedDays < 2) {
-      return `${timeElapsedDays} day ago`;
-    } 
-    else if(timeElapsedDays >= 2){
-      return `${timeElapsedDays} days ago`;
-    }
-    else if (timeElapsedHours > 0 && timeElapsedHours < 2) {
-      return `${timeElapsedHours} hour ago`;
-    } 
-    else if (timeElapsedHours >= 2) {
-      return `${timeElapsedHours} hours ago`;
-    }else if (timeElapsedMinutes > 0 && timeElapsedMinutes < 2 ) {
-      return `${timeElapsedMinutes} minute ago`;
-    } else if (timeElapsedMinutes >= 2) {
-      return `${timeElapsedMinutes} minutes ago`;
-    }else {
-      return `${timeElapsedSeconds} seconds ago`;
-    }
-  }
-
-  const formatDateTime = (dateTime) => {
-    const options = {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    };
-    const formatter = new Intl.DateTimeFormat('en-US', options);
-    return formatter.format(dateTime);
-  } 
-
-  const determineColor = (remake, outcome) =>{
-    if(remake) return "remake"
-    return outcome ? "victory":"defeat"
-  }
-
-  const stringCapitalize = (matchOutcome)=>{
-    return matchOutcome.charAt(0).toUpperCase() + matchOutcome.slice(1)
-  }
+ 
--- public/helper.js
+++ public/helper.js
@@ -1,3 +1,61 @@
-export const foo = (bar) =>{
-    console.log(bar)
-}
+export const getTimeAgo = (timestamp) => {
+    // Get the current time in milliseconds
+    const currentTime = new Date().getTime();
+    
+    // Calculate the time elapsed since the game started
+    const timeElapsed = currentTime - timestamp;
+    
+    // Convert the time elapsed to seconds
+    const timeElapsedSeconds = Math.floor(timeElapsed / 1000);
+    
+    // Convert the time elapsed to minutes
+    const timeElapsedMinutes = Math.floor(timeElapsedSeconds / 60);
+    
+    // Convert the time elapsed to hours
+    const timeElapsedHours = Math.floor(timeElapsedMinutes / 60);
+    
+    // Convert the time elapsed to days
+    const timeElapsedDays = Math.floor(timeElapsedHours / 24);
+    
+    // Return the time elapsed as a human-readable string
+    if (timeElapsedDays > 0 && timeElapsedDays < 2) {
+      return `${timeElapsedDays} day ago`;
+    } 
+    else if(timeElapsedDays >= 2){
+      return `${timeElapsedDays} days ago`;
+    }
+    else if (timeElapsedHours > 0 && timeElapsedHours < 2) {
+      return `${timeElapsedHours} hour ago`;
+    } 
+    else if (timeElapsedHours >= 2) {
+      return `${timeElapsedHours} hours ago`;
+    }else if (timeElapsedMinutes > 0 && timeElapsedMinutes < 2 ) {
+      return `${timeElapsedMinutes} minute ago`;
+    } else if (timeElapsedMinutes >= 2) {
+      return `${timeElapsedMinutes} minutes ago`;
+    }else {
+      return `${timeElapsedSeconds} seconds ago`;
+    }
+  }
+
+  export const formatDateTime = (dateTime) => {
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    };
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+    return formatter.format(dateTime);
+  } 
+
+ export  const determineColor = (remake, outcome) =>{
+    if(remake) return "remake"
+    return outcome ? "victory":"defeat"
+  }
+
+  export const stringCapitalize = (matchOutcome)=>{
+    return matchOutcome.charAt(0).toUpperCase() + matchOutcome.slice(1)
+  }
